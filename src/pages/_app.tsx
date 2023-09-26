ef5e8063c8d034ffc9728f9a5501175dd8c17b1b
Rostislav Alekseev <mr.rostik.01@gmail.com>
2023-09-26 15:52:12 +0200

    loading spinner while SSR page loads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,24 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { store } from "@/store";
 import { Provider } from "react-redux";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+import Loading from "@/components/Loading";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [isLoading, setData] = useState(false);
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      setData(true);
+    });
+    router.events.on("routeChangeComplete", () => {
+      setData(false);
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,7 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Provider store={store}>
-        <Component {...pageProps} />
+        {isLoading ? <Loading /> : <Component {...pageProps} />}
       </Provider>
     </>
   );
